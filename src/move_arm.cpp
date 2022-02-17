f4c8766cc3ff3699cbaaa0805fefe5f60eabe2c7
Catherine Ding <catherineding28@gmail.com>
2022-02-17 14:18:46 -0500

    first commit

---
+++ src/move_arm.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <signal.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <cstdlib>
+#include <std_msgs/String.h>
+
+int main(int argc, char **argv) {
+	// Intialize ROS with this node name
+	ros::init(argc, argv, "move_arm");
+
+	ros::NodeHandle n;
+	
+
+	return 0;
+}
